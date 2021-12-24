5ef80f484e3b5792307e3b58a8ebe13781a3a1bb
Menci <huanghaorui301@gmail.com>
2021-12-24 05:47:56 +0000

    Set larger simulation time

--- run.sh
+++ run.sh
@@ -4,8 +4,8 @@
 cd sim
 
 # 运行仿真程序
-# +vcs+finish: 使仿真在 5us 后自动退出
+# +vcs+finish: 使仿真在 50000us 后自动退出
 # +fsdbfile: 指定导出波形文件的名称为 wave.fsdb
 # -q: 安静模式（减少日志数量）
-./simv +vcs+finish+500us +fsdbfile+wave.fsdb -q
+./simv +vcs+finish+50000us +fsdbfile+wave.fsdb -q
 
